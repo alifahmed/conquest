15833d5140a047088c33f51dfeab29f7bb9b7555
Alif Ahmed <aliflaila420@gmail.com>
2017-08-30 19:23:14 -0400

    Added cb_aes

--- test/AES/AES-T1100/conquest_tb.v
+++ test/AES/AES-T1100/conquest_tb.v
@@ -5,9 +5,8 @@ module conquest_tb();
     reg  rst;
     reg  [127:0] state = 128'b0;
     reg  [127:0] key = 128'b0;
-    wire [127:0] out;
-    wire [63:0] Capacitance;
-    reg  __obs;
+    wire [127:0] out = 128'b0;
+    wire [63:0] Capacitance = 64'b0;
 
     // Generated top module instance
     top _conc_top_inst(
@@ -16,13 +15,12 @@ module conquest_tb();
             .state     ( state ),
             .key       ( key ),
             .out       ( out ),
-            .Capacitance( Capacitance ),
-            .__obs     ( __obs ));
+            .Capacitance( Capacitance ));
 
     // Generated internal use signals
     reg  [31:0] _conc_pc;
-    reg  [256:0] _conc_opcode;
-    reg  [256:0] _conc_ram[0:10];
+    reg  [255:0] _conc_opcode;
+    reg  [255:0] _conc_ram[0:10];
 
 
     // Generated clock pulse
@@ -34,7 +32,6 @@ module conquest_tb();
     always @(posedge clk) begin
         _conc_pc = _conc_pc + 32'b1;
         _conc_opcode = _conc_ram[_conc_pc];
-        __obs <= #1 _conc_opcode[256];
         key <= #1 _conc_opcode[255:128];
         state <= #1 _conc_opcode[127:0];
         $strobe(";_C %d", _conc_pc);
@@ -42,10 +39,14 @@ module conquest_tb();
 
     // Generated initial block
     initial begin
+        $display(";_C 1");
         clk = 1'b0;
         rst = 1'b0;
-        _conc_pc = 32'b0;
+        _conc_pc = 32'b1;
         $readmemb("data.mem", _conc_ram);
+        _conc_opcode = _conc_ram[1];
+        key <= #1 _conc_opcode[255:128];
+        state <= #1 _conc_opcode[127:0];
         #2 clk = 1'b1;
         rst = 1'b1;
         #5 rst = 1'b0;
